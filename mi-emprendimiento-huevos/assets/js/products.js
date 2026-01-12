894a34789abebeef361f62c06c4b1e2cdbf10744
PabloRoulet8 <pablo.roulet12@gmail.com>
2026-01-12 15:08:01 -0300

    modificacion
    elimine el dacto de docena de huevo, cambie el alias de transferencia

--- mi-emprendimiento-huevos/assets/js/products.js
+++ mi-emprendimiento-huevos/assets/js/products.js
@@ -2,10 +2,6 @@
 // Modifica los valores aquí y se actualizarán en toda la web.
 
 const PRODUCTS_DB = {
-    "docena": {
-        name: "Docena Colorada",
-        price: 4200, 
-    },
     "maple": {
         name: "Maple (30 Unid.)",
         price: 9500,
